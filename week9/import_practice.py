a5eff3313ee9d8ff74400cedb6a685f16f2bfeef
York MacBook 020 <jhuang523@gmail.com>
2024-01-25 12:11:40 -0600

    week9

---
+++ week9/import_practice.py
@@ -0,0 +1,12 @@
+class Test:
+    def __init__(self):
+        print("Made an object")
+
+class Test2:
+    def __init__(self, x):
+        print(x)
+
+
+def test_method():
+    print("imported test_method")
+    
